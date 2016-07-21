6a7e6fb6636d21be2d11a18ea0ed234a6b8da198
Cyril Yu <cyrilyu@diuit.com>
2016-07-21 15:31:23 +0800

    refactor models and routes

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
-var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
+var mongoose  = require('mongoose');
+var Schema    = mongoose.Schema;
 var config    = require('./config');
+var Promise   = require('bluebird');
+var diuitauth = require('diuit-auth');
+var hash      = require('password-hash');
 
 // user schema
 var User = new Schema({
@@ -12,7 +15,80 @@ var User = new Schema({
 {
   timestamps: true
 });
+
+var client = {
+  'appId': config.appid,
+  'appKey': config.appkey,
+  'encryptionKeyId': config.keyid,
+  'encryptionKey': config.privatekey.replace(/\\n/g, '\n'),
+  'exp' : 60*60*24*7,
+  'platform': config.platformtype
+};
+
+// Model Static Method
+User.statics.signup = function (username, password) {
+  return new Promise((resolve, reject) => {
+    return this.findOne({ username: username }).then(function (user) {
+      if (user) {
+        reject("user existed");
+      } else {
+        client.userSerial = "user." + username;
+        client.deviceSerial = client.userSerial +  ".device.0";
+        return diuitauth.getSessionToken(client)
+      }
+    }).then(function (resp) {
+      let sessionExpiredDate = new Date();
+      sessionExpiredDate.setDate(sessionExpiredDate.getDate() + 7);
+      let passwd = hash.generate(password);
+      let session  = resp.session;
+      resolve({
+        "username": username,
+        "password": passwd,
+        "session_token": session,
+        "session_expiredAt": sessionExpiredDate
+      })
+    }).catch(function (e) {
+      reject(e)
+    })
+  })
+}
+
+User.statics.signin = function (username, password) {
+  return new Promise((resolve, reject) => {
+    let userObj = null;
+    return this.findOne({username: username}).then(function (user) {
+      if (!user) {
+        reject("user not exist")
+      }
+      if (!hash.verify(password, user.password)) {
+        reject("password mismatch")
+      }
+      // return session_token if session not expired
+      if (user.session_expiredAt > Date.now()) {
+        console.log(user.session_expiredAt)
+        resolve(user)
+      } else {
+        userObj = user
+        client.userSerial = "user." + username;
+        client.deviceSerial = client.userSerial +  ".device.0";
+        return diuitauth.getSessionToken(client)
+      }      
+    }).then(function (resp) {
+      console.log(resp)
+      let sessionExpiredDate = new Date();
+      sessionExpiredDate.setDate(sessionExpiredDate.getDate() + 7);
+      userObj.session_token     = resp.session;
+      userObj.session_expiredAt = sessionExpiredDate;
+      return userObj.save();
+    }).then(function (user) {
+      resolve(user);
+    }).catch(function (e) {
+      reject(e)
+    })
+  })
+}
+
 //use schema create a module, means create an model named 'User'
-mongoose.model('User', User);
+module.exports = mongoose.model('User', User);
 //connect db
-mongoose.connect(config.dburi);
+// mongoose.connect(config.dburi);
--- routes/index.js
+++ routes/index.js
@@ -1,109 +1,56 @@
 var express   = require('express');
 var hash      = require('password-hash');
 var mongoose  = require('mongoose');
-var User      = mongoose.model('User');
+// var User      = mongoose.model('User');
+var User = require('../db')
 mongoose.Promise = require('bluebird');
-var diuitauth = require('diuit-auth');
 var config    = require('../config');
 var router    = express.Router();
 
-var client = {
-  'appId': config.appid,
-  'appKey': config.appkey,
-  'encryptionKeyId': config.keyid,
-  'encryptionKey': config.privatekey.replace(/\\n/g, '\n'),
-  'exp' : 60*60*24*7,
-  'platform': config.platformtype
-};
+var ROUTES = {
+  SIGNUP: '/signup'
+  , SIGNIN: '/signin'
+}
 
-/* user sign up */
-router.post('/signup', function(req, res, next) {
-
-  req.checkBody('username', 'Invalid username').notEmpty()
-  req.checkBody('password', 'Invalid password').notEmpty()
+router.post(ROUTES.SIGNUP, function(req, res, next) {
+  req.checkBody('username', 'Invalid username').notEmpty();
+  req.checkBody('password', 'Invalid password').notEmpty();
+  console.log(req.body.password)
   if (err = req.validationErrors()) {
     res.status(400).send(err)
-    return;
+    return
   }
-
-  const spec = req.body;
-
-  client.userSerial = "user." + spec.username;
-  client.deviceSerial = client.userSerial +  ".device.0";
-
-  var promise = User.findOne({username: spec.username}).exec();
-  promise.then(function(user) {
-    if (user != null) {
-      throw new Error("user existed");
-    }
-
-    return diuitauth.getSessionToken(client);
-  }).then(function(resp){
-    console.log("get session from Diuit API server:" + resp.session);
-    // hash password
-    spec.password = hash.generate(spec.password);
-    spec.session = resp.session;
-    var sessionExpiredDate = new Date();
-    sessionExpiredDate.setDate(sessionExpiredDate.getDate() + 7);
-
-    return new User({
-        "username": spec.username,
-        "password": spec.password,
-        "session_token": spec.session,
-        "session_expiredAt": sessionExpiredDate
-    }).save();
-  }).then(function(){
-    res.status(200).json({"session":spec.session});
-  }).catch(function(error){
-    console.log("error:" + error);
-    res.status(499).json({"error": error});
-  });
-});
+  let username = req.body.username;
+  let password = req.body.password;
+  User.signup(username, password).then(function (result) {
+    let user = new User(result)
+    return user.save()
+  }).then(function (user) {
+    console.log(user.session_token)
+    res.status(200).json({session: user.session_token})
+  }).catch(function (e) {
+    console.log(e)
+    res.status(499).json({error: e})
+  })
+})
 
 /* user sign in */
-router.post('/signin', function(req, res, next) {
-  const spec = req.body;
+router.post(ROUTES.SIGNIN, function(req, res, next) {
+  req.checkBody('username', 'Invalid username').notEmpty();
+  req.checkBody('password', 'Invalid password').notEmpty();
+  if (err = req.validationErrors()) {
+    res.status(400).send(err)
+    return
+  }
+  let username = req.body.username;
+  let password = req.body.password;
 
-  var promise = User.findOne({username: spec.username}).exec();
-  var resultUser = null;
-  promise.then(function(user) {
-    if(user == null) {
-      throw new Error("user not exist");
-    }
-
-    if(!hash.verify(spec.password, user.password)) {
-      throw new Error("password mismatch");
-    }
-
-    if(user.session_expiredAt > Date.now()) {
-      console.log("found user:"+ user);
-      res.status(200).json({"session":user.session_token});
-    } else {
-      resultUser = user;
-      console.log("refresh session because it's expired");
-      client.userSerial = "user." + spec.username;
-      client.deviceSerial = client.userSerial +  ".device.0";
-
-      diuitauth.getSessionToken(client)
-      .then(function(resp) {
-        console.log("new session token:" + resp.session);
-        user.session_token = resp.session;
-        var sessionExpiredDate = new Date();
-        sessionExpiredDate.setDate(sessionExpiredDate.getDate() + 7);
-        user.session_expiredAt = sessionExpiredDate;
-        return user.save();
-      }).then(function(user) {
-        res.status(200).json({"session": resultUser.session_token});
-      }).catch(function(error) {
-        console.log("error:" + error);
-        res.status(499).json({"error":error});
-      })
-    }
-  }).catch(function(error){
-    console.log("error:" + error);
-    res.status(499).json({"error": error});
+  User.signin(username, password).then(function (user) {
+    res.status(200).json({session: user.session_token})
+  }).catch(function (e) {
+    console.log(e);
+    res.status(499).json({error : e});
   })
-
 });
 
 module.exports = router;
